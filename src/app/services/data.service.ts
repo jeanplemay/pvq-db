693e878c05dfe535cc83cc8afdf16d2927cb676c
Jean-Philippe Lemay <info@jplemay.com>
2024-02-24 07:48:36 -0500

    Test fix build

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import * as data from 'data/pvq-db.json';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -8,8 +10,17 @@ export class DataService {
 
     private data: any;
 
-    constructor() {
-        this.data = data;
+    constructor(public http: HttpClient) {
+        this.getData();
+    }
+
+    private getPromise(file: string): Promise<any> {
+        const salt = (new Date()).getTime();
+        return lastValueFrom(this.http.get<any>(environment.apiUrl + 'data/' + file + '?' + salt));
+    }
+
+    private async getData(): Promise<any> {
+        this.data = await this.getPromise("pvq-db.json");
     }
 
     public getCompes(): any {
